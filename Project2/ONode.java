770487f25a0daf5b75be1eb680d0a52a502c9f7e
JamesAndrew <jandrewshields@gmail.com>
2015-09-27 16:28:05 -0600

    Revert "Added sigmoid and tanh functions"
    This reverts commit 3987d479c8a16889035aea28f180b8681c92ad65.

--- Project2/ONode.java
+++ Project2/ONode.java
@@ -3,23 +3,4 @@ public class ONode
 {
 	private float[] weight;
 	private float output;
-	
-		/**
-	 *	An activation function using a sigmoid function. 
-	 *
-	 */
-	public float sigActivate(){
-		float e = (float) Math.E;
-        float t = (float) Math.pow(e, -output);
-        float activate = 1 / (1 + t);
-		return activate;
-	}
-	
-	/**
-	 *  An activation function using a hyperbolic tangent.
-	 */
-	public float tanhActivate(){
-        float activate = (float) Math.tanh(output);
-        return activate;
-    }
 }
