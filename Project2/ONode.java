211acc5df010a44a2036c66d894405536b9ec4d0
avDavis <jellie_fry@msn.com>
2015-09-26 21:10:52 -0600

    Initial addition of starting code
    James' start on MLP and RandomDist, also includes a  new license doc.

---
+++ Project2/ONode.java
@@ -0,0 +1,6 @@
+
+public class ONode 
+{
+	float[] weight;
+	float output;
+}
---
+++ Project2/RandomDist.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class RandomDist 
+{
+	float[] ranDist;
+	float min;
+	float max;
+	
+	RandomDist(int size)
+	{
+		ranDist = new float[size];
+	    min = -5.0f;
+		max = 5.0f;
+		getDist();
+	}
+	
+	// method to construct a uniform random distribution of real numbers
+	float[] getDist()
+	{
+		Random randomN = new Random();
+		for (int i = 0; i < ranDist.length; i++)
+		{
+			ranDist[i] =  randomN.nextFloat();
+		}
+		
+		return ranDist;
+	}
+	
+	// method to display distribution 
+	void printDist()
+	{
+		for (int i = 0; i < ranDist.length; i++)
+		{
+			System.out.println(ranDist[i]);
+		}
+	}
+}
